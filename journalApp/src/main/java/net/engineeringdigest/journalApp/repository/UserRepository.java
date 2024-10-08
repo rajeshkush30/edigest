551f59373d4822e0d0d84c21f9e4dea01372bfff
Rajesh Kushwaha <rajeshrsk30@gmail.com>
2024-10-08 18:36:47 +0530

    test2

--- journalApp/src/main/java/net/engineeringdigest/journalApp/repository/UserRepository.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, ObjectId> {
-     int num=100;
+     int num=500;
      User findByUserName(String username);
+
 }
